08955cf27de9c1d58f2d60ca8772aa3931fbca3b
dcode.localhost <dcode.localhost@gmail.com>
2025-01-05 13:23:44 +0530

    demo files

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple JavaScript to handle form submission
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Your message has been sent!');
+    // Here you would typically handle the form submission, like sending data to a server.
+});
